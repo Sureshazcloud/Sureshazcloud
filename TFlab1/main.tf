9b320b100a339ad7a1744768c245cc91b0926aa4
Sureshazcloud <104635655+Sureshazcloud@users.noreply.github.com>
2022-11-26 23:41:53 -0500

    Update main.tf

--- TFlab1/main.tf
+++ TFlab1/main.tf
@@ -1,57 +1,35 @@
-resource "azurerm_resource_group" "RG-Terraform" {
-  name     = "terraform-resource-group"
-  location = "West Europe"
-}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
 
-resource "azurerm_app_service_plan" "ASP-TerraForm" {
-  name                = "terraform-appserviceplan"
-  location            = azurerm_resource_group.RG-Terraform.location
-  resource_group_name = azurerm_resource_group.RG-Terraform.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
+ backend "azurerm" {
+    resource_group_name  = "TFbuild"
+    storage_account_name = "sureshtfstate"
+    container_name       = "tfstate"
+    key                  = "demo.terraform.tfstate"
   }
 }
 
-resource "azurerm_app_service" "AS-Terraform" {
-  name                = "app-service-terraform"
-  location            = azurerm_resource_group.RG-Terraform.location
-  resource_group_name = azurerm_resource_group.RG-Terraform.name
-  app_service_plan_id = azurerm_app_service_plan.ASP-TerraForm.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
-
-  connection_string {
-    name  = "Database"
-    type  = "SQLServer"
-    value = "Server=tcp:${azurerm_sql_server.test.fully_qualified_domain_name} Database=${azurerm_sql_database.test.name};User ID=${azurerm_sql_server.test.administrator_login};Password=${azurerm_sql_server.test.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
-  }
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  # subscription_id = var.subscription_id
+  # client_id       = var.client_id
+  # client_secret   = var.client_secret
+  # tenant_id       = var.tenant_id
 }
 
-resource "azurerm_sql_server" "test" {
-  name                         = "terraform-sqlserver"
-  resource_group_name          = azurerm_resource_group.RG-Terraform.name
-  location                     = azurerm_resource_group.RG-Terraform.location
-  version                      = "12.0"
-  administrator_login          = "houssem"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
-}
 
-resource "azurerm_sql_database" "test" {
-  name                = "terraform-sqldatabase"
-  resource_group_name = azurerm_resource_group.RG-Terraform.name
-  location            = azurerm_resource_group.RG-Terraform.location
-  server_name         = azurerm_sql_server.test.name
-
-  tags = {
-    environment = "production"
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = "demo-rg"
+  location = "East US"
+  tags =  {
+    env = "dev"
+    cost = "TA102AXY"
   }
 }
